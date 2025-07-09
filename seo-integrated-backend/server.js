7c0685722a28667e14a70463eeb2b832098a46e8
Shyambss <167905083+Shyambss@users.noreply.github.com>
2025-07-09 21:30:55 +0530

    Update server.js

--- seo-integrated-backend/server.js
+++ seo-integrated-backend/server.js
@@ -26,10 +26,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend's origin
-  credentials: true, // Allow cookies to be sent with requests
-}));
+app.use(cors());
 app.use(express.json()); // Parse JSON request bodies
 
 // Connect to MongoDB
